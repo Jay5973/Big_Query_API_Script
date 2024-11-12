0d55748f5b39f338d7f3c9442b77257b03161d75
Jay5973 <jay.bhararia@collegedunia.com>
2024-11-12 13:49:59 +0530

    Update one-astro-main.py

--- one-astro-main.py
+++ one-astro-main.py
@@ -101,7 +101,7 @@ class UniqueUsersProcessor:
     def process_chat_accepted_events(self):
         intake_events = self.raw_df[self.raw_df['event_name'] == 'chat_intake_submit']
         valid_user_ids = intake_events['user_id'].unique()
-        accept_events = self.raw_df[(self.raw_df['event_name'] == 'accept_chat') & (self.raw_df['paid'] == 'false') & (self.raw_df['clientId'].isin(valid_user_ids))]
+        accept_events = self.raw_df[(self.raw_df['event_name'] == 'accept_chat') & (self.raw_df['paid'] == 0) & (self.raw_df['clientId'].isin(valid_user_ids))]
         accept_events['event_time'] = pd.to_datetime(accept_events['event_time'], utc=True) + pd.DateOffset(hours=5, minutes=30)
         accept_events['date'] = accept_events['event_time'].dt.date
         accept_events['hour'] = accept_events['event_time'].dt.hour
