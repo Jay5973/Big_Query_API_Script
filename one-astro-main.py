17a86762200d52417e5152c70781f3596e02d1b5
Jay5973 <jay.bhararia@collegedunia.com>
2024-11-18 18:24:49 +0530

    Update one-astro-main.py

--- one-astro-main.py
+++ one-astro-main.py
@@ -611,11 +611,11 @@ hasClicked = card(
 
 astros_live_1 = processor.astros_live_1()
 print(astros_live_1)
-# astros_live_1_str = str(astros_live_1['astros_live_1'].tail(1).values[0])
-# hasClicked = card(
-#   title=astros_live_1_str,  # Now passing a string to the title
-#   text="Astrologers Live Currently"
-# )
+astros_live_1_str = str(astros_live_1['astros_live_1'].tail(1).values[0])
+hasClicked = card(
+  title=astros_live_1_str,  # Now passing a string to the title
+  text="Astrologers Live Currently"
+)
 
 
 # # Process each event type
